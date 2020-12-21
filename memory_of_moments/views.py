c1483229d9fe2def8b373bf38abbc45fd274026f
RifatRobin <rifatitna@gmail.com>
2020-12-21 23:28:14 +0600

    urls,views,settings are okay. due to exam time couldnot have much time to spend in the project now

--- memory_of_moments/views.py
+++ memory_of_moments/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def login(request):
+    return render(request, "/login.html")
+
+
+def register(request):
+    return render(request, "/login.html")
+
+
+def add(request):
+    return render(request, "/login.html")
+
+
+def pre(request):
+    return render(request, "/login.html")
--- moments/settings.py
+++ moments/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'memory_of_moments.app.MemoryOfMomentsConfig',
+    'memory_of_moments.apps.MemoryOfMomentsConfig',
 ]
 
 MIDDLEWARE = [
